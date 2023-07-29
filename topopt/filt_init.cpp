258febe4d895a82d5eb0bd32379176e1b134ca9a
Dirk Munro <dirkmunro8@gmail.com>
2023-07-30 00:12:40 +0200

    in work

--- topopt/filt_init.cpp
+++ topopt/filt_init.cpp
@@ -1,73 +1,60 @@
 #include "main.h"
 #include <set>
 //
-int filt_init(int n_e, MatrixXi& els, MatrixXd& nds, SpMat& H, int lvl)
+int filt_init(int n_e, MatrixXi& els, MatrixXd& nds, SpMat& H, int lvl, int nze)
 {
 //
   std::vector<T> coefs;
-  if(lvl == 0){
-    cout<<"No filtering"<<endl;
-    coefs.reserve(n_e);
-  }else if(lvl==1){
-    cout<<"Level 1 filtering; reserving 28 entries per element."<<endl;
-    coefs.reserve(28*n_e);
-  }else if(lvl==2){
-    cout<<"Level 2 filtering; reserving 153 entries per element."<<endl;
-    coefs.reserve(153*n_e);
-  }else if(lvl==3){
-    cout<<"Level 3 filtering; reserving 496 entries per element."<<endl;
-    coefs.reserve(496*n_e);
-  }else{
-    cout<<"Filtering level unknown; not reserving entries."<<endl;
-  }
+  coefs.reserve(nze*n_e);
+//
   ArrayXi con_e;
   MatrixXi noc_e;
 //
-//  lvl = 0 -> nothing
-//  lvl = 1 -> filter immediate neighbours
+//make inverse of connect matrix, matrix of centroid coordinates, and get length scale
 //
-//  lvl=2;
+  MatrixXi noc = -1*MatrixXi::Ones(nds.rows(), 9);
+  ArrayXi cnt = VectorXi::Zero(nds.rows());
+  MatrixXd cen = MatrixXd::Zero(n_e,3);
+  VectorXd cmp;
+  double len = 1e8;
+  VectorXd cmp_0;
+  VectorXd Hsum = VectorXd::Zero(n_e);
 //
-// make inverse of connect matrix, matrix of centroid coordinates, and get length scale
+//this is not needed with my filtering:
 //
-    MatrixXi noc = -1*MatrixXi::Ones(nds.rows(), 9);
-    ArrayXi cnt = VectorXi::Zero(nds.rows());
-    MatrixXd cen = MatrixXd::Zero(n_e,3);
-    VectorXd cmp;
-    double len = 1e8;
-    VectorXd cmp_0;
-    VectorXd Hsum = VectorXd::Zero(n_e);
-//
-    for(int e = 0; e < n_e; e++){
-        con_e = els(e,Eigen::all); 
-        cmp = VectorXd::Zero(3);
-        for(int i = 0; i < con_e.size(); i++){
-            noc(con_e(i),cnt(con_e(i))) = e;
-            cmp = cmp + nds(con_e(i),Eigen::all).transpose()/con_e.size();
-        }
-        cen(e,Eigen::all) = cmp;
-        if(e==0){
-            cmp_0 = cmp;
-        }else{
-            len = min(len, (cmp-cmp_0).norm());
-        }
-        cnt(con_e) = cnt(con_e) + 1;
-        if ((cnt(con_e) == 9).any()){
-            std::cout << "ERROR" << endl;
-        }
+  for(int e = 0; e < n_e; e++){
+    con_e = els(e,Eigen::all); 
+    cmp = VectorXd::Zero(3);
+    for(int i = 0; i < con_e.size(); i++){
+      noc(con_e(i),cnt(con_e(i))) = e;
+      cmp = cmp + nds(con_e(i),Eigen::all).transpose()/con_e.size();
     }
+    cen(e,Eigen::all) = cmp;
+    if(e==0){
+      cmp_0 = cmp;
+    }else{
+      len = min(len, (cmp-cmp_0).norm());
+    }
+    cnt(con_e) = cnt(con_e) + 1;
+    if((cnt(con_e) == 9).any()){
+      std::cout << "ERROR" << endl;
+    }
+  }
 //
-// for each element
+//for each element
 //
-    double krn;
-    double sum=0.;
-    for(int e = 0; e < n_e; e++){
+  double krn;
+  double sum_max=0.;
+  int c_max=0;
+  for(int e = 0; e < n_e; e++){
 //
+      int c_e=0;
       double sum_e=0.;
       std::set<int> q0; 
       q0.insert(e);
       krn = (float) lvl+1.0;
       coefs.push_back(T(e, e, krn));
+      c_e=c_e+1;
       sum_e=sum_e+krn;
 //
 //     for each level
@@ -95,21 +82,22 @@ int filt_init(int n_e, MatrixXi& els, MatrixXd& nds, SpMat& H, int lvl)
             krn = (float)lvl - l;
             coefs.push_back(T(e, *it, krn));
             sum_e=sum_e+krn;
+            c_e=c_e+1;
           }
-//       add everything to q0 for next lelvel
+//       add everything to q0 for next level
           q0.insert(qn.begin(),qn.end());
         }
-      sum=fmax(sum_e,sum);
+      sum_max=fmax(sum_e,sum_max);
+      c_max=max(c_e,c_max);
     }
 //
-    cout <<"Max. number of non-zeros associated with an element: "<<sum << endl;
+//  cout <<"Max. number of non-zeros associated with an element: "<<c_max<< endl;
 //
     for(int e = 0; e < n_e; e++){
-        Hsum(e)=sum;
+        Hsum(e)=sum_max;
     }
     H.setFromTriplets(coefs.begin(), coefs.end());
     H = H * Hsum.asDiagonal().inverse();
-    cout <<"Number of non-zeros in filter matrix: "<<H.nonZeros() << endl;
 //
 return 0;
 }
--- topopt/main.cpp
+++ topopt/main.cpp
@@ -12,13 +12,17 @@ int main(int argc, char *argv[])
 //time
 //
   auto t0 = high_resolution_clock::now();
+  auto t1 = high_resolution_clock::now();
+  auto t2 = high_resolution_clock::now();
+  auto t21 = duration_cast<milliseconds>(t2-t1).count();
 //
 //check input
 //
   printf("======================================================================\n");
   if (argc == 9) {
-    cout<<"Input: "<<endl<<
-    " 1 - number of threads: "<<argv[1]<<endl<<
+    printf("Input\n");
+    printf("--------------------------------------------------------------------\n");
+    cout<<" 1 - number of threads: "<<argv[1]<<endl<<
     " 2 - solver: "<<argv[2]<<endl<<
     " 3 - mesh file: "<<argv[3]<<endl<<
     " 4 - finite diff.: "<<argv[4]<<endl<<
@@ -46,14 +50,14 @@ int main(int argc, char *argv[])
   int fdc; std::stringstream str3(argv[4]); str3 >> fdc;
   double opt_vf; std::stringstream str4(argv[5]); str4 >> opt_vf;
   int nca; std::stringstream str5(argv[6]); str5 >> nca;
-  int lvl; std::stringstream str6(argv[7]); str6 >> lvl;
+  flt._lvl; std::stringstream str6(argv[7]); str6 >> flt._lvl;
   double eps; std::stringstream str7(argv[8]); str7 >> eps;
 //
 //set threads
 //
   Eigen::setNbThreads(thr);
 //
-//initializations
+//generic initializations
 //
   int e, i, i1, i2, j, j1, j2, k, c, err, gss;
   int nnz, n_d, n_e, n_n, n_p, n_f;
@@ -75,26 +79,51 @@ int main(int argc, char *argv[])
 //
 //read input deck (json currently)
 //
-  auto t1 = high_resolution_clock::now();
-  read_json(argv[3], nds, els, &gss, mat, mat_aux, bds, lds, opt._ex);
-  auto t2 = high_resolution_clock::now(); 
-  auto dur_inp = duration_cast<milliseconds>(t2 - t1).count();
   printf("======================================================================\n");
-  std::cout << "Input data parsed (" << dur_inp << ")\n";
+  printf("Mesh\n"); 
+  printf("--------------------------------------------------------------------\n");
 //
-//reserve estimate of number of non-zeros
-//... and define some other things ...
+  printf(" file: %s\n",argv[3]);
 //
+  t1 = high_resolution_clock::now();
+  read_json(argv[3], nds, els, &gss, mat, mat_aux, bds, lds, opt._ex);
+  t2 = high_resolution_clock::now(); t21 = duration_cast<milliseconds>(t2-t1).count();
+//
+  printf(" read in %7.1e seconds.\n",(double)t21/1000.);
   nnz = pow(els.cols()*nds.cols(),2)*els.rows();
-  n_e = els.rows(); // number of elements
-  n_n = nds.rows(); // number of nodes
-  n_d = nds.cols()*nds.rows(); // number of dofs
-  n_p = bds.rows(); // number of prescribed dofs 
-  n_f = n_d-n_p; // free dofs
-  cout << "- Elements: "<<n_e <<endl;
-  cout << "- Nodes: "<<n_n <<endl;
-  cout << "- Dofs: "<<n_d <<endl;
-  cout << "- Free: "<<n_f <<endl;
+  n_e = els.rows(); 
+  n_n = nds.rows(); 
+  n_d = nds.cols()*nds.rows();
+  n_p = bds.rows(); 
+  n_f = n_d-n_p;
+  printf(" elements: %d\n",n_e);
+  printf(" nodes: %d\n",n_n);
+  printf(" dofs: %d\n",n_d);
+  printf(" prescribed: %d\n",n_p);
+  printf(" free: %d\n",n_f);
+  printf(" reserved nnz: %d\n",nnz);
+//
+//filter init.
+//
+  printf("======================================================================\n");
+  printf("Filtering\n");
+  printf("--------------------------------------------------------------------\n");
+//
+  printf(" levels: %d\n",flt._lvl);
+//
+  flt._ro = VectorXd::Zero(n_e);
+  flt._H = SpMat(n_e,n_e);
+  flt._nze= (2*flt._lvl+1)*(2*flt._lvl+1)*(2*flt._lvl+1); //NON_ZERO ESTIMATE FOR STRUCTURED GRID!
+//
+  t1 = high_resolution_clock::now();
+  err = filt_init(n_e, els, nds, flt._H, flt._lvl, flt._nze);
+  t2 = high_resolution_clock::now(); t21 = duration_cast<milliseconds>(t2-t1).count();
+//
+  printf(" filter matrix made in %7.1e seconds\n",(double)t21/1000.);
+  printf(" estimated nnz per element: %d\n",flt._nze);
+  printf(" reserved nnz: %d\n",flt._nze*n_e);
+  printf(" actual nnz: %ld\n",flt._H.nonZeros()); 
+  printf(" filled: %4.2f\n",(double)flt._H.nonZeros()/n_e/n_e);
 //
 //opt. init.
 //
@@ -122,12 +151,7 @@ int main(int argc, char *argv[])
   acc._la = opt._la;
   double a = 1.0;
 //
-//filter init.
-//
-  printf("======================================================================\n");
-  flt._ro = VectorXd::Zero(n_e);
-  flt._H = SpMat(n_e,n_e);
-  err=filt_init(n_e, els, nds, flt._H, lvl);
+//linear system init.
 //
 //dof vectors containing 
 // - loads
@@ -240,128 +264,128 @@ int main(int argc, char *argv[])
 //
 //  scaling objective and its sensitivities
 //
-        if(itr==0){
-            opt_scl = opt_obj;
-        }
-        opt_obj=opt_obj/opt_scl;
-        opt_sns1=opt_sns1/opt_scl;
+    if(itr==0){
+      opt_scl = opt_obj;
+    }
+    opt_obj=opt_obj/opt_scl;
+    opt_sns1=opt_sns1/opt_scl;
 //
-        opt._ef(0) = opt_obj;
-        opt._ef(1) = -flt._ro.sum()/n_e/opt_vf + 1.;
+    opt._ef(0) = opt_obj;
+    opt._ef(1) = -flt._ro.sum()/n_e/opt_vf + 1.;
 //
 //  calculate delta f
 //
-        opt_def = opt._ef(0) - acc._ef(0);
+    opt_def = opt._ef(0) - acc._ef(0);
 //
 //  check if feasible descent step
 //
-        if(opt_def > 0. && fdc==0){
+    if(opt_def > 1e9 && fdc==0){
 //
-//      retrieve the last acceptable iterate and objective value / do not update subproblem
+//    retrieve the last acceptable iterate and objective value / do not update subproblem
 //
-            opt._ex = acc._ex;
-            opt._ef = acc._ef; // just for output
-            a=2.0*a;
+      opt._ex = acc._ex;
+      opt._ef = acc._ef; // just for output
+      a=2.0*a;
 //
+    }else{
+//
+//    construct subproblem
+//
+      if(fdc){
+        if(itr == 0){
+          fdc_ef0=opt._ef;
+          opt._df(0,all) = opt_sns1;
+          opt._df(1,all) = opt_sns2;
         }else{
-//
-//      construct subproblem
-//
-            if(fdc){
-                if(itr == 0){
-                    fdc_ef0=opt._ef;
-                    opt._df(0,all) = opt_sns1;
-                    opt._df(1,all) = opt_sns2;
-                }else{
-                    VectorXd opt_err = VectorXd::Zero(2);
-                    printf("%8.1e ", fdx);
-                    for(int j = 0; j<2; j++){
-                        opt_err(j) = ((opt._ef(j)-fdc_ef0(j))/fdx - opt._df(j,fdi))/opt._df(j,fdi);
-                        printf("%14.7e ", opt_err(j));
-                    }
-                    printf("\n");
-                }
-            }else{
-//
-//              spherical curvature
-//
-//		double eps = 1e-6;
-                double tmp; double nrm;
-                opt._df(0,all) = opt_sns1;
-                opt._df(1,all) = opt_sns2;
-                opt._cf(0,all) = eps*VectorXd::Ones(n_e);
-                if(itr>0){
-                  nrm = (opt._ex-opt._exh).squaredNorm();
-		          if(nca){
-                    tmp=2.*fabs(opt._efh(0)-opt._ef(0)-opt._df(0,all).dot(opt._exh-opt._ex))/nrm;
-		    }else{
-                    	tmp=2.*(opt._efh(0)-opt._ef(0)-opt._df(0,all).dot(opt._exh-opt._ex))/nrm;
-	            }
-                    opt._cf(0,all) = (tmp*VectorXd::Ones(n_e)).cwiseMax(eps);
-//                  opt._cf(0,all) = -2.0*opt_sns.cwiseProduct(opt._ex.cwiseInverse());
-                }
-                opt._cf(1,all) = VectorXd::Zero(n_e);
-//
-//              subproblem bounds
-//
-                opt._dxl = opt._xl.cwiseMax(opt._ex - 0.1*(opt._xu-opt._xl));
-                opt._dxu = opt._xu.cwiseMin(opt._ex + 0.1*(opt._xu-opt._xl));
-//
-//              save this acceptable iterate
-//
-                acc._ex = opt._ex; acc._df = opt._df; acc._cf = opt._cf;
-                acc._dxl = opt._dxl; acc._dxu = opt._dxu; acc._la = opt._la;
-                opt._exh=opt._ex; acc._ef = opt._ef; a=1.0;
-            }
+          VectorXd opt_err = VectorXd::Zero(2);
+          printf("%8.1e ", fdx);
+          for(int j = 0; j<2; j++){
+            opt_err(j) = ((opt._ef(j)-fdc_ef0(j))/fdx - opt._df(j,fdi))/opt._df(j,fdi);
+            printf("%14.7e ", opt_err(j));
+          }
+          printf("\n");
         }
+      }else{
+//
+//      spherical curvature
+//
+        double tmp; double nrm;
+        opt._df(0,all) = opt_sns1;
+        opt._df(1,all) = opt_sns2;
+        opt._cf(0,all) = eps*VectorXd::Ones(n_e);
+        if(itr>0){
+          nrm = (opt._ex-opt._exh).squaredNorm();
+		  if(nca){
+            tmp=2.*fabs(opt._efh(0)-opt._ef(0)-opt._df(0,all).dot(opt._exh-opt._ex))/nrm;
+		  }else{
+            tmp=2.*(opt._efh(0)-opt._ef(0)-opt._df(0,all).dot(opt._exh-opt._ex))/nrm;
+	      }
+          opt._cf(0,all) = (tmp*VectorXd::Ones(n_e)).cwiseMax(eps);
+//        opt._cf(0,all) = -2.0*opt_sns.cwiseProduct(opt._ex.cwiseInverse());
+        }
+        opt._cf(1,all) = VectorXd::Zero(n_e);
+//
+//      subproblem bounds
+//
+        opt._dxl = opt._xl.cwiseMax(opt._ex - 0.1*(opt._xu-opt._xl));
+        opt._dxu = opt._xu.cwiseMin(opt._ex + 0.1*(opt._xu-opt._xl));
+//
+//      save this acceptable iterate
+//
+        acc._ex = opt._ex; acc._df = opt._df; acc._cf = opt._cf;
+        acc._dxl = opt._dxl; acc._dxu = opt._dxu; acc._la = opt._la;
+        opt._exh=opt._ex; acc._ef = opt._ef; a=1.0;
+      }
+    }
 //
 //  solve subproblem and update design variables
 //
-        double opt_dx = 0.;
-        VectorXd opt_nx = VectorXd::Zero(n_e);
-        if(fdc){
-            fdx = 1e-16*pow(10.0,itr);
-            opt._ex = 0.5*VectorXd::Ones(n_e);
-            opt._ex(fdi) = opt._ex(fdi) + fdx;
-        }else{
-            MatrixXd tmp_cf = acc._cf*a;
-            err=dqpsub(acc._ex, acc._ef, acc._df, tmp_cf, acc._dxl, acc._dxu, opt._cs, acc._la, opt_nx);
-            opt_dx = (opt_nx - opt._ex).array().abs().maxCoeff();
-            opt._ex = opt_nx; opt._efh = opt._ef;
-        }
+    double opt_dx = 0.;
+    VectorXd opt_nx = VectorXd::Zero(n_e);
+    if(fdc){
+      fdx = 1e-16*pow(10.0,itr);
+      opt._ex = 0.5*VectorXd::Ones(n_e);
+      opt._ex(fdi) = opt._ex(fdi) + fdx;
+    }else{
+      MatrixXd tmp_cf = acc._cf*a;
+      err=dqpsub(acc._ex, acc._ef, acc._df, tmp_cf, acc._dxl, acc._dxu, opt._cs, acc._la, opt_nx);
+      opt_dx = (opt_nx - opt._ex).array().abs().maxCoeff();
+      opt._ex = opt_nx; opt._efh = opt._ef;
+    }
 //
 //  write output
 //
-        t1 = high_resolution_clock::now();
-        wrte_lvtk(argv[3],itr,nds,els,gss,mat,mat_aux,dfs_sol,dfs_pre,dfs_lds,opt._ex,flt._ro);
-        t2 = high_resolution_clock::now();
-        auto dur_out = duration_cast<milliseconds>(t2 - t1).count();
-        auto dur_itr = duration_cast<milliseconds>(t2 - ti).count();
-        auto dur_tot = duration_cast<milliseconds>(t2 - t0).count();
-        if(fdc){
-            if(itr==18){
-                break;
-            }
-        }else{
-            double opt_baw = 0.0;
-            for(int j = 0; j<n_e;j++){
-                if( opt._ex(j) < opt._xl(j) + 1e-3 ){
-                    opt_baw = opt_baw + 1.0/n_e;
-                }else if ( opt._ex(j) > opt._xu(j) - 1e-3) {
-                    opt_baw = opt_baw + 1.0/n_e;
-                }
-            }
-            printf("%3d %10.3e %10.3e %8.1e %8.1e %8.1e %8.1e %8.1e\n", itr, opt._ef(0)*opt_scl, 
-                opt._ef(1), opt_dx, opt_def, opt_baw, (double)dur_itr/1000, (double)dur_tot/1000.);
+    wrte_lvtk(argv[3],itr,nds,els,gss,mat,mat_aux,dfs_sol,dfs_pre,dfs_lds,opt._ex,flt._ro);
+//
+//  time, black and white, and screen output
+//
+    t2 = high_resolution_clock::now();
+    auto dur_itr = duration_cast<milliseconds>(t2 - ti).count();
+    auto dur_tot = duration_cast<milliseconds>(t2 - t0).count();
+    if(fdc){
+      if(itr==18){
+        break;
+      }
+    }else{
+      double opt_baw = 0.0;
+      for(int j = 0; j<n_e;j++){
+        if( opt._ex(j) < opt._xl(j) + 1e-3 ){
+          opt_baw = opt_baw + 1.0/n_e;
+        }else if ( opt._ex(j) > opt._xu(j) - 1e-3) {
+          opt_baw = opt_baw + 1.0/n_e;
         }
-        fflush(stdout);
+      }
+      printf("%3d %10.3e %10.3e %8.1e %8.1e %8.1e %8.1e %8.1e\n", itr, opt._ef(0)*opt_scl, 
+        opt._ef(1), opt_dx, opt_def, opt_baw, (double)dur_itr/1000, (double)dur_tot/1000.);
+    }
+    fflush(stdout);
 //
 //  possibly terminate
 //
-        if(fdc==0 && opt_dx < 1e-6 && a < 1.5 && itr > 100 ){
-            break;
-        }
-//
+    if(fdc==0 && opt_dx < 1e-2 && a < 1.5 && fabs(opt_def) < 1e-6 ){
+      break;
     }
+//
+  }
 //
 }
